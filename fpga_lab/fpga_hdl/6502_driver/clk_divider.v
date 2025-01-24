171622faf9fc3eb08206a93dc34c4226e56b3609
EmulationOnline <alex@emulationonline.com>
2025-01-23 20:46:33 -0800

    Clock divider: Take divisor as input instead of parameter.
    Verilator doesn't allow changing parameters at runtime. This makes
    testing challenging. Instead allow each divider to be configured during
    reset.

--- fpga_lab/fpga_hdl/6502_driver/clk_divider.v
+++ fpga_lab/fpga_hdl/6502_driver/clk_divider.v
@@ -1,13 +1,16 @@
-module clk_divider #(parameter int DIVISOR = 2) (
+module clk_divider (
     input clk, reset,
+    [31:0] divisor,
     output reg divided
 );
     // Divides full clock cycles by a given multiple. 
+    reg [31:0] DIVISOR;
     reg [31:0] CTR;  // TODO: set to appropriate length based on divisor
 
     always @(posedge clk) begin
         if (reset) begin
             CTR <= 32'h0;
+            DIVISOR <= divisor;
             divided <= 0;
         end
         else if (CTR +1 == DIVISOR) begin
--- fpga_lab/fpga_hdl/6502_driver/test_clk_divider.cc
+++ fpga_lab/fpga_hdl/6502_driver/test_clk_divider.cc
@@ -4,17 +4,19 @@
 // Verilator generated code
 #include "obj_dir/Vclk_divider.h"
 
-
-// Divide by 2 is fixed
-TEST(ClkDivider, Div2) {
+void test_divider(int division) {
     auto ctx = std::make_unique<VerilatedContext>();
     auto top = std::make_unique<Vclk_divider>(ctx.get());
 
+    // Rest
     top->reset = 1;
+    top->divisor = division;
     top->clk = 0;
     top->eval();
-    top->reset = 0;
+
+    top->clk = 1;
     top->eval();
+    top->reset = 0;
 
     auto count_state = [&](bool val) -> int{
         int count = 0;
@@ -39,11 +41,14 @@ TEST(ClkDivider, Div2) {
 
     // Test for 3 output cycles
     for (int i = 0; i < 3; i++) {
-        ASSERT_EQ(2, count_state(0));
-        ASSERT_EQ(2, count_state(1));
+        ASSERT_EQ(division, count_state(0));
+        ASSERT_EQ(division, count_state(1));
     }
-
 }
+
+TEST(ClkDivider, Div2) { test_divider(2); }
+TEST(ClkDivider, Div3) { test_divider(3); }
+TEST(ClkDivider, Div4) { test_divider(4); }
 
 // TODO: set parameter and test other divisors
 // TEST(ClkDivider, Div4) {
