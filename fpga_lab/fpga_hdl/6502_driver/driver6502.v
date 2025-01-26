bed48e9bad64beba6ffb105173afdb4867ae2235
EmulationOnline <alex@emulationonline.com>
2025-01-25 16:38:59 -0800

    Basic outline for 6502_driver.

--- fpga_lab/fpga_hdl/6502_driver/driver6502.v
+++ fpga_lab/fpga_hdl/6502_driver/driver6502.v
@@ -2,5 +2,16 @@ module top(
     input clk, reset
 );
 
+    reg [15:0]ADDRESS;
+
+    always @(posedge clk) begin
+    end
+
+    SB_SPRAM256KA ram(
+        .ADDRESS(ADDRESS),
+        .DATAIN(DATA),
+        .MASKWREN(4'b1111)
+    );
+
 endmodule
 
